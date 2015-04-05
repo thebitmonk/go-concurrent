d2ca9f1dfcc5e69d6b6cad60068206c185e535dc
Varun Jain <varun@indix.com>
2015-04-05 22:58:39 +0530

    Adding boring function that return channel to read messages. Channels are first class citizens.

--- boring/boring.go
+++ boring/boring.go
@@ -6,13 +6,23 @@ import (
 	"time"
 )
 
+func asyncBoring(msg string) <-chan string {
+	c := make(chan string)
+	go func() {
+		for i := 0; ; i++ {
+			fmt.Println(msg, i)
+			c <- fmt.Sprintf("%s %d", msg, i)
+			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+		}
+	}()
+	return c
+}
+
 func boring(msg string, c chan string) {
 	for i := 0; ; i++ {
 		fmt.Println(msg, i)
 		c <- fmt.Sprintf("%s %d", msg, i)
-		// time.Sleep(time.Second)
 		time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
-
 	}
 }
 
@@ -22,6 +32,12 @@ func main() {
 	fmt.Println("I am listening")
 	for i := 0; i < 5; i++ {
 		fmt.Printf("You say %q\n", <-c)
+	}
+	fmt.Println(" You are boring. Ditching ya !")
+
+	asyncBoring("Hey !")
+	for i := 0; i < 5; i++ {
+		fmt.Printf("You said %q\n", <-c)
 	}
 	fmt.Println(" You are boring. Ditching ya !")
 }
