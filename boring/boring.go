c9895ebce700ce5b7467cf2864c45e35df268f7e
Varun Jain <varun@indix.com>
2015-04-05 23:51:30 +0530

    Adding fan in function. Multiplexed communication.

--- boring/boring.go
+++ boring/boring.go
@@ -1,43 +1,44 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
+import "fmt"
 
-func asyncBoring(msg string) <-chan string {
+func boring(msg string) <-chan string {
 	c := make(chan string)
 	go func() {
 		for i := 0; ; i++ {
-			fmt.Println(msg, i)
 			c <- fmt.Sprintf("%s %d", msg, i)
-			time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
+			//time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
 		}
 	}()
 	return c
 }
 
-func boring(msg string, c chan string) {
-	for i := 0; ; i++ {
-		fmt.Println(msg, i)
-		c <- fmt.Sprintf("%s %d", msg, i)
-		time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
-	}
+func fanIn(input1, input2 <-chan string) <-chan string {
+	c := make(chan string)
+	go func() {
+		for {
+			c <- <-input1
+		}
+	}()
+	go func() {
+		for {
+			c <- <-input2
+		}
+	}()
+	return c
 }
 
 func main() {
-	c := make(chan string)
-	go boring("Hello", c)
-	fmt.Println("I am listening")
-	for i := 0; i < 5; i++ {
-		fmt.Printf("You say %q\n", <-c)
-	}
-	fmt.Println(" You are boring. Ditching ya !")
+	// joe := boring("Joe")
+	// ann := boring("Ann")
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println(<-joe)
+	// 	fmt.Println(<-ann)
+	// }
+	c := fanIn(boring("Joe"), boring("Ann"))
 
-	asyncBoring("Hey !")
-	for i := 0; i < 5; i++ {
-		fmt.Printf("You said %q\n", <-c)
+	for i := 0; i < 10; i++ {
+		fmt.Println(<-c)
 	}
 	fmt.Println(" You are boring. Ditching ya !")
 }
